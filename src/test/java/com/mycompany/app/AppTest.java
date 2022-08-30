6797c779132b85156285a75f40c64ba3f0af28d6
Sid-1620 <66297189+Sid-1620@users.noreply.github.com>
2022-08-30 17:06:46 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,9 +34,9 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello World Siddharth M!" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello World Siddharth M!\"");
+            fail("\"message\" is not \"Hello World!\"");
         }
     }
 
